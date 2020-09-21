ebde2d410f678143020b4be5f5a53a81b374ae5e
KimHS0915 <remember2552@gmail.com>
2020-09-22 00:24:29 +0900

    Update timeline

--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,11 @@
+from datetime import timedelta
+
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
 
 from .models import Post
 from .forms import PostForm
@@ -10,10 +13,15 @@ from .forms import PostForm
 
 @login_required
 def index(request):
-    post_list = Post.objects.all().filter(
-        Q(author=request.user) |
-        Q(author__in=request.user.following_set.all())
-    )
+    timesince = timezone.now() - timedelta(days=3)
+    post_list = Post.objects.all()\
+        .filter(
+            Q(author=request.user) |
+            Q(author__in=request.user.following_set.all())
+        )\
+        .filter(
+            created_at__gte=timesince
+        )
     suggested_user_list = get_user_model().objects.exclude(pk=request.user.pk).exclude(pk__in=request.user.following_set.all())[:3]
     return render(request, "instagram/index.html", {
         "post_list": post_list,
